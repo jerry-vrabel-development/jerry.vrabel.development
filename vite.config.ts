9c9823a846ce409060c2deabba07de474df2e8d3
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2025-04-03 17:59:20 -0500

    updated base dir

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jerry-vrabel-development.github.io/',
+  base: 'https://jerry-vrabel-development.github.io/jerry.vrabel.development/',
 })
