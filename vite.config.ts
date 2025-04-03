e3354d1c86607c501e80285a338e0daadd15497c
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2025-04-03 18:16:01 -0500

    CI config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/jerry-vrabel-development/jerry.vrabel.development.git/'
+  base: '/https://github.com/jerry-vrabel-development/jerry.vrabel.development/'
 })
