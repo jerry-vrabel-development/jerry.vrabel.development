295d716c32915b0026749388214ae6afc1850ea7
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2025-04-03 16:43:14 -0500

    FIRST

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import bashLogo from '/250-bash.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Jerry Vrabel Development</h1>
+        <p>Web Application Development</p>
+        <a href="" target="_blank">
+          <img src={bashLogo} className="logo" alt="Bash logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
